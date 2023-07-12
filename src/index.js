f991ca6e88f527c1e4e2906e4dbcf4d63399f0bc
liuting <liu.ting@luban.com>
2023-07-12 15:14:36 +0800

    perf: 去除mock功能

--- src/index.js
+++ src/index.js
@@ -1,13 +1,11 @@
 const transform = require("./transform.js");
 const message = require("./message.js");
 const transformC = require("./transform-clipboard.js");
-const mockClipboardData = require("./mock-clipboard-data.js");
 
 function activate(context) {
 	context.subscriptions.push(message);
     context.subscriptions.push(transform);
 	context.subscriptions.push(transformC);
-	context.subscriptions.push(mockClipboardData);
 }
 
 function deactivate() {}
